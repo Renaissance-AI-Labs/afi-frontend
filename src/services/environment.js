b1a6eb459e1d0a56ea9aba515ceb05c844dd3163
blackcats-shadow <BlackcatsParadise@outlook.com>
2026-06-12 17:32:23 +0800

    Update whitelist to include an additional address for improved access control.

--- src/services/environment.js
+++ src/services/environment.js
@@ -40,7 +40,8 @@ export const GLOBAL_DASHBOARD_WHITELIST = [
   // Add whitelisted addresses here
   '0xdd8c7d63fa18faefba74be22e69cfa43c7bbe6d6',
   '0x9B651D652B747ba7821f27B92Dd65c68b9dd30C8',
-  '0x56063c541a05f320519e7af9ac5d43becb2ea699'
+  '0x56063c541a05f320519e7af9ac5d43becb2ea699',
+  '0x502b565F317E26376c0B63369BfDee262BC75D7D'
 ].map(addr => addr.toLowerCase());
 
 /**
